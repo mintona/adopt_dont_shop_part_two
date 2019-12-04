4b90719da4d48476f8b471fdfe69f1125c8089b1
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-04 15:11:33 -0700

    Add functionality for edit and update reviews
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,6 +17,17 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @shelter = Shelter.find(params[:shelter_id])
+    @review = Review.find(params[:review_id])
+  end
+
+  def update
+    review = Review.find(params[:review_id])
+    review.update(review_params)
+    redirect_to "/shelters/#{params[:shelter_id]}"
+  end
+
   private
   def review_params
     params.permit(:title, :rating, :content, :image)
