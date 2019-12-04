8afd01e9081e158d9e41b5065a99b7be0013e609
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-04 11:49:26 -0700

    Add logic for flash message notice and success
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,12 +6,19 @@ class ReviewsController < ApplicationController
   def create
     review = Review.new(review_params)
     review.shelter_id = params[:shelter_id]
-    review.save!
-    redirect_to "/shelters/#{params[:shelter_id]}"
+
+    if review.save
+      flash[:success] = 'Your review has been posted.'
+      redirect_to "/shelters/#{params[:shelter_id]}"
+    else
+      flash[:notice] = "Please fill out all required fields."
+      @shelter_id = params[:shelter_id]
+      render :new
+    end
   end
 
   private
-    def review_params
-      params.permit(:title, :rating, :content, :image)
-    end
+  def review_params
+    params.permit(:title, :rating, :content, :image)
+  end
 end
