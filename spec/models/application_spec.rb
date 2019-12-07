6d318b4562e31a43ffa281c4695e6164e9a97c19
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-07 14:22:22 -0700

    Validate phone number
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Application, type: :model do
     it {should validate_presence_of :city}
     it {should validate_presence_of :state}
     it {should validate_presence_of :zip}
+    it {should validate_presence_of :phone}
     it {should validate_presence_of :description}
   end
 
