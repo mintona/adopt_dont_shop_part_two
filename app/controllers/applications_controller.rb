9cd08fa248b3c4ffe248b98e6bc7aaba4b45efe8
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-07 12:38:51 -0700

    Initial attempt at create method and setup of private application_params method
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,4 +5,23 @@ class ApplicationsController < ApplicationController
       Pet.find(id.to_i)
     end
   end
+
+  def create
+    require "pry"; binding.pry
+    selected_pets = params[:selected_pets]
+    application = Application.new(application_params)
+    #take this ! out at the end
+    application.save!
+
+    redirect_to '/favorites'
+  end
+
+  private
+    def application_params
+      params.permit(:name, :address, :city, :state, :zip, :description)
+    end
+
+    # def selected_pets
+    #   params.permit(:selected_pets)
+    # end
 end
