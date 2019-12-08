725910fdae8c3639951848e80a9cff4a5891eb3c
Alison Vermeil <alison.minton@gmail.com>
2019-12-08 16:35:19 -0700

    Refactor applications controller to use active record better and have flash notice for no pet selected

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,34 +1,37 @@
 class ApplicationsController < ApplicationController
   def new
-    pet_ids = favorites_list.contents.keys
-    @pets = pet_ids.map do |id|
-      Pet.find(id.to_i)
-    end
+    pet_ids = favorites_list.pet_ids
+    @pets = Pet.where(id: pet_ids)
   end
 
   def create
     application = Application.new(application_params)
-    if application.save
+
+    if application.save && params[:selected_pets] == nil
+      flash[:notice] = "Please select at least one pet."
+      pet_ids = favorites_list.pet_ids
+      @pets = Pet.where(id: pet_ids)
+      render :new
+
+    elsif application.save
       selected_pet_ids = params[:selected_pets]
-      @selected_pets = selected_pet_ids.map do |id|
-        Pet.find(id.to_i)
-      end
+      @selected_pets = Pet.where(id: selected_pet_ids)
 
       @selected_pets.each do |pet|
         pet.applications << application
       end
-
+      # next method can be refactored somehow I'm sure in favorites_list PORO
       selected_pet_ids.each do |id|
         favorites_list.remove(id)
       end
+
       flash[:success] = "You have applied to adopt your selected pets."
       redirect_to '/favorites'
+
     else
       flash[:notice] = "Please fill out all fields."
-      pet_ids = favorites_list.contents.keys
-      @pets = pet_ids.map do |id|
-        Pet.find(id.to_i)
-      end
+      pet_ids = favorites_list.pet_ids
+      @pets = Pet.where(id: pet_ids)
       render :new
     end
   end
