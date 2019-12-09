a0a13f759d12cd244430bb532ccc6dba802eb594
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-09 14:15:33 -0700

    Add test for applications index off of pet show page
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -1,83 +1,71 @@
-# require 'rails_helper'
-#
-# RSpec.describe "As a visitor" do
-#   # describe "after one or more applications have been created" do
-#   #   before(:each) do
-#   #     @shelter_1 = Shelter.create(name: "Boulder Shelter",
-#   #                               address: "123 Arapahoe Ave",
-#   #                               city: "Boulder",
-#   #                               state: "CO",
-#   #                               zip: "80301")
-#   #
-#   #     pet_1_image = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
-#   #     pet_1_description = "I am a loveable, snuggly, cat! If you are anti snuggle, look elsewhere. I want to be pet at all times!"
-#   #
-#   #     @pet_1 = Pet.create(image: pet_1_image,
-#   #                         name: "Alex",
-#   #                         approximate_age: "10",
-#   #                         sex: "Male",
-#   #                         description: pet_1_description,
-#   #                         shelter: @shelter_1)
-#   #
-#   #     @shelter_2 = Shelter.create(name: "Denver Shelter",
-#   #                               address: "345 Blake St",
-#   #                               city: "Denver",
-#   #                               state: "CO",
-#   #                               zip: "80220")
-#   #
-#   #     pet_2_image = "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg"
-#   #     pet_2_description = 'I am the cutest puppy ever! I love to be around kids as long as they do not play too "ruff."'
-#   #     @pet_2 = Pet.create(image: pet_2_image,
-#   #                         name: "Marley",
-#   #                         approximate_age: "2",
-#   #                         sex: "Female",
-#   #                         description: pet_2_description,
-#   #                         shelter: @shelter_2)
-#   #
-#   #     pets = [@pet_1, @pet_2]
-#   #
-#   #     pets.each do |pet|
-#   #       visit "/pets/#{pet.id}"
-#   #       click_button 'Add to Favorite Pets'
-#   #     end
-#   #
-#   #     application = Application.create!(name: 'Jordan Holtkamp',
-#   #                                       address: '123 Main St',
-#   #                                       city: 'Lafayette',
-#   #                                       state: 'CO',
-#   #                                       zip: '80515',
-#   #                                       phone: '6102021418',
-#   #                                       description: 'I am a great pet dad.')
-#   #   end
-#   #
-#   #   describe "when I visit the favorites index page" do
-#   #     it "I see a section that lists all pets with at least one application" do
-#   #       visit '/favorites'
-#   #
-#   #       within '#favorite-pets' do
-#   #         expect(page).to have_content(@pet_1.name)
-#   #         expect(page).to have_css("img[src*='#{@pet_1.image}']")
-#   #
-#   #         expect(page).to have_content(@pet_2.name)
-#   #         expect(page).to have_css("img[src*='#{@pet_2.image}']")
-#   #       end
-#   #     end
-#   #
-#   #     xit "I can click a pet name to visit the pet show page" do
-#   #       visit '/favorites'
-#   #
-#   #       within "#favorite-pets" do
-#   #         click_link "#{@pet_1.name}"
-#   #         expect(current_path).to eq("/pets/#{@pet_1.id}")
-#   #       end
-#   #
-#   #       visit '/favorites'
-#   #
-#   #       within "#favorite-pets" do
-#   #         click_link "#{@pet_2.name}"
-#   #         expect(current_path).to eq("/pets/#{@pet_1.id}")
-#   #       end
-#   #     end
-#   #   end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  before(:each) do
+    @shelter_1 = Shelter.create(name: "Boulder Shelter",
+                              address: "123 Arapahoe Ave",
+                              city: "Boulder",
+                              state: "CO",
+                              zip: "80301")
+
+    pet_1_image = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
+    pet_1_description = "I am a loveable, snuggly, cat! If you are anti snuggle, look elsewhere. I want to be pet at all times!"
+
+    @pet_1 = Pet.create(image: pet_1_image,
+                        name: "Alex",
+                        approximate_age: "10",
+                        sex: "Male",
+                        description: pet_1_description,
+                        shelter: @shelter_1)
+
+    pet_2_image = "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg"
+    pet_2_description = 'I am the cutest puppy ever! I love to be around kids as long as they do not play too "ruff."'
+    @pet_2 = Pet.create(image: pet_2_image,
+                        name: "Marley",
+                        approximate_age: "2",
+                        sex: "Female",
+                        description: pet_2_description,
+                        shelter: @shelter_1)
+
+    pets = [@pet_1, @pet_2]
+
+    pets.each do |pet|
+      visit "/pets/#{pet.id}"
+      click_button 'Add to Favorite Pets'
+    end
+
+    application = Application.create!(name: 'Jordan Holtkamp',
+                                      address: '123 Main St',
+                                      city: 'Lafayette',
+                                      state: 'CO',
+                                      zip: '80515',
+                                      phone: '6102021418',
+                                      description: 'I am a great pet dad.')
+
+    application_2 = Application.create!(name: 'Ali Vermeil',
+                                      address: '100 Larimer St',
+                                      city: 'Denver',
+                                      state: 'CO',
+                                      zip: '80211',
+                                      phone: '3309078495',
+                                      description: 'I am a great pet mom and my animal just got hit by a car.')
+    @pet_1.applications << application
+    @pet_2.applications << application
+    @pet_1.applications << application_2
+  end
+
+  describe "When I visit a pet show page" do
+    describe "When I click the link to view all applications" do
+      it "I see a list of all the names of applicants for the pet" do
+        visit "/pets/#{@pet_1.id}"
+
+        click_button "View all applications for #{@pet_1.name}"
+
+        expect(current_path).to eq("/pets/#{@pet_1.id}/applications")
+
+        expect(page).to have_link('Jordan Holtkamp')
+        expect(page).to have_link('Ali Vermeil')
+      end
+    end
+  end
+end
