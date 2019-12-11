c2a9bfc1b5a15c36668a415d466e8e389b542412
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-11 13:16:39 -0700

    Add tests for model methods for stats

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -58,5 +58,126 @@ describe Shelter, type: :model do
         expect(@shelter_1.pets_pending?).to eq(true)
       end
     end
+
+    describe '#average_rating' do
+      it "gives the average rating of the shelter" do
+        shelter_1 = Shelter.create(name: "Boulder Shelter",
+                                   address: "123 Arapahoe Ave",
+                                   city: "Boulder",
+                                   state: "CO",
+                                   zip: "80301")
+
+        shelter_2 = Shelter.create(name: "Denver Shelter",
+                                   address: "345 Blake St",
+                                   city: "Denver",
+                                   state: "CO",
+                                   zip: "80220")
+
+        review_1 = Review.create!(title: "Great shelter!",
+                                 rating: 3,
+                                 content: "I got the best lil pup from this place and the staff was super helpful!",
+                                 image: "http://rmpuppyrescue.org/images/1052801_529279620453288_1859974512_o%20(1)172x119_2x.jpg",
+                                 shelter: shelter_1)
+
+        review_2 = Review.create!(title: "Cute dogs!",
+                                 rating: 4,
+                                 content: "I got the cutest pit bull, but the front desk lady was kind of mean.",
+                                 image: "http://rmpuppyrescue.org/images/spay%20clinic-crop-u1009034_2x.jpg",
+                                 shelter: shelter_1)
+
+        review_3 = Review.create!(title: "The best!",
+                                 rating: 4,
+                                 content: "I love kitty cats",
+                                 image: "https://face4pets.org/wp-content/uploads/2015/06/shelter-cat3.jpg",
+                                 shelter: shelter_2)
+
+        expect(shelter_1.average_rating).to eq(3.50)
+      end
+    end
+
+    describe "#pet_count" do
+      it "counts the pets at a shelter" do
+        shelter_1 = Shelter.create(name: "Boulder Shelter",
+                                   address: "123 Arapahoe Ave",
+                                   city: "Boulder",
+                                   state: "CO",
+                                   zip: "80301")
+
+        shelter_2 = Shelter.create(name: "Denver Shelter",
+                                   address: "345 Blake St",
+                                   city: "Denver",
+                                   state: "CO",
+                                   zip: "80220")
+
+         pet_1_image = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
+         pet_1_description = "I am a loveable, snuggly, cat! If you are anti snuggle, look elsewhere. I want to be pet at all times!"
+
+         pet_1 = Pet.create!(image: pet_1_image,
+                             name: "Alex",
+                             approximate_age: "10",
+                             sex: "Male",
+                             shelter: shelter_1,
+                             description: pet_1_description)
+
+         pet_2_image = "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg"
+         pet_2_description = 'I am the cutest puppy ever! I love to be around kids as long as they do not play too "ruff."'
+         pet_2 = Pet.create!(image: pet_2_image,
+                             name: "Marley",
+                             approximate_age: "8 weeks",
+                             sex: "Female",
+                             shelter: shelter_1,
+                             description: pet_2_description)
+
+        expect(shelter_1.pet_count).to eq(2)
+        expect(shelter_2.pet_count).to eq(0)
+      end
+    end
+
+    describe '#number_of_applications' do
+      it "counts the number of applications on file for a shelter" do
+        pet_1_image = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
+        pet_1_description = "I am a loveable, snuggly, cat! If you are anti snuggle, look elsewhere. I want to be pet at all times!"
+
+        shelter_1 = Shelter.create(name: "Boulder Shelter",
+                                   address: "123 Arapahoe Ave",
+                                   city: "Boulder",
+                                   state: "CO",
+                                   zip: "80301")
+        pet_1 = Pet.create(image: pet_1_image,
+                           name: "Alex",
+                           approximate_age: "10",
+                           sex: "Male",
+                           description: pet_1_description,
+                           shelter: shelter_1)
+
+        application_1 = Application.create!(name: 'Jordan Holtkamp',
+                                            address: '123 Main St',
+                                            city: 'Lafayette',
+                                            state: 'CO',
+                                            zip: '80515',
+                                            phone: '6102021418',
+                                            description: 'I am a great pet dad.')
+        pet_1.applications << application_1
+
+        application_2 = Application.create!(name: 'Ali Vermeil',
+                                            address: '123 Blake St',
+                                            city: 'Lafayette',
+                                            state: 'CO',
+                                            zip: '80515',
+                                            phone: '3309078495',
+                                            description: 'I am a great pet mom.')
+
+        application_3 = Application.create!(name: 'Ali Vermeil',
+                                            address: '123 Harvard St',
+                                            city: 'Lafayette',
+                                            state: 'CO',
+                                            zip: '80515',
+                                            phone: '3309078495',
+                                            description: 'I am a great pet mom.')
+        pet_1.applications << application_2
+
+        expect(shelter_1.number_of_applications).to eq(2)
+      end
+    end
   end
 end
