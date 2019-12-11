69d3a25a80a711c7ce2a4c439c3c1d1b6c342e23
Alison Vermeil <alison.minton@gmail.com>
2019-12-10 20:18:12 -0700

    Update destroy action to have flash message if pet has approved app

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -35,8 +35,15 @@ class PetsController < ApplicationController
   end
 
   def destroy
-    Pet.destroy(params[:id])
-    redirect_to '/pets'
+    pet = Pet.find(params[:id])
+    if pet.approved_application?
+      flash[:notice] = "#{pet.name} has an approved application and cannot be deleted."
+      @pet = Pet.find(params[:id])
+      render :show
+    else
+      Pet.destroy(params[:id])
+      redirect_to '/pets'
+    end
   end
 
   def update_adoptable
